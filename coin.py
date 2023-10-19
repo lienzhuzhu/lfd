9e1fc882bd3606fdd3b779b7d4cc7ef7c4ee3d4a
Lien Zhu <lienzhucs@gmail.com>
2023-10-18 23:51:50 -0700

    plots hoeffding

--- coin.py
+++ coin.py
@@ -28,23 +28,83 @@ for i in range(TRIALS):
 
 
 # Plotting the results
-plt.figure(figsize=(15,5))
+#plt.figure(figsize=(15,5))
+#
+#plt.subplot(1, 3, 1)
+#plt.hist(first_coin_heads, bins=range(12), alpha=0.7, edgecolor='black')
+#plt.title("First Coin")
+#plt.xlabel("Number of Heads")
+#plt.ylabel("Frequency")
+#
+#plt.subplot(1, 3, 2)
+#plt.hist(random_coin_heads, bins=range(12), alpha=0.7, edgecolor='black')
+#plt.title("Randomly Chosen Coin")
+#plt.xlabel("Number of Heads")
+#
+#plt.subplot(1, 3, 3)
+#plt.hist(min_heads_coin_heads, bins=range(12), alpha=0.7, edgecolor='black')
+#plt.title("Coin with Minimum Heads")
+#plt.xlabel("Number of Heads")
+#
+#plt.tight_layout()
+#plt.show()
 
-plt.subplot(1, 3, 1)
+
+
+def hoeffding_bound(epsilon, N):
+    return 2 * np.exp(-2 * epsilon**2 * N)
+
+epsilons = np.arange(0, 0.5, 0.01)
+
+first_coin_probs = [np.mean(np.abs(first_coin_heads/FLIPS - 0.5) > eps) for eps in epsilons]
+random_coin_probs = [np.mean(np.abs(random_coin_heads/FLIPS - 0.5) > eps) for eps in epsilons]
+min_heads_coin_probs = [np.mean(np.abs(min_heads_coin_heads/FLIPS - 0.5) > eps) for eps in epsilons]
+
+hoeffding_probs = hoeffding_bound(epsilons, FLIPS)
+
+
+
+
+# Plotting the results
+plt.figure(figsize=(15, 15))
+
+# Histograms
+plt.subplot(3, 3, 1)
 plt.hist(first_coin_heads, bins=range(12), alpha=0.7, edgecolor='black')
-plt.title("First Coin")
+plt.title("First Coin Histogram")
 plt.xlabel("Number of Heads")
 plt.ylabel("Frequency")
 
-plt.subplot(1, 3, 2)
+plt.subplot(3, 3, 2)
 plt.hist(random_coin_heads, bins=range(12), alpha=0.7, edgecolor='black')
-plt.title("Randomly Chosen Coin")
+plt.title("Randomly Chosen Coin Histogram")
 plt.xlabel("Number of Heads")
 
-plt.subplot(1, 3, 3)
+plt.subplot(3, 3, 3)
 plt.hist(min_heads_coin_heads, bins=range(12), alpha=0.7, edgecolor='black')
-plt.title("Coin with Minimum Heads")
+plt.title("Coin with Minimum Heads Histogram")
 plt.xlabel("Number of Heads")
+
+# Hoeffding Plots
+plt.subplot(3, 3, 4)
+plt.plot(epsilons, first_coin_probs, label="Experimental")
+plt.plot(epsilons, hoeffding_probs, label="Hoeffding Bound", linestyle='--')
+plt.title("First Coin")
+plt.xlabel("Epsilon (ε)")
+plt.ylabel("Probability Measure")
+plt.legend()
+
+plt.subplot(3, 3, 5)
+plt.plot(epsilons, random_coin_probs, label="Experimental")
+plt.plot(epsilons, hoeffding_probs, label="Hoeffding Bound", linestyle='--')
+plt.title("Randomly Chosen Coin")
+plt.xlabel("Epsilon (ε)")
+
+plt.subplot(3, 3, 6)
+plt.plot(epsilons, min_heads_coin_probs, label="Experimental")
+plt.plot(epsilons, hoeffding_probs, label="Hoeffding Bound", linestyle='--')
+plt.title("Coin with Minimum Heads")
+plt.xlabel("Epsilon (ε)")
 
 plt.tight_layout()
 plt.show()
