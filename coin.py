c07edbb2dd581500a0b73ef9a04085408f45d479
Lien Zhu <lienzhucs@gmail.com>
2023-10-19 19:49:33 -0700

    cleans code

--- coin.py
+++ coin.py
@@ -9,6 +9,18 @@ first_coin_heads = np.zeros(TRIALS)
 random_coin_heads = np.zeros(TRIALS)
 min_heads_coin_heads = np.zeros(TRIALS)
 
+epsilons = np.arange(0, 0.5, 0.01)
+
+first_coin_probs = [np.mean(np.abs(first_coin_heads/FLIPS - 0.5) > eps) for eps in epsilons]
+random_coin_probs = [np.mean(np.abs(random_coin_heads/FLIPS - 0.5) > eps) for eps in epsilons]
+min_heads_coin_probs = [np.mean(np.abs(min_heads_coin_heads/FLIPS - 0.5) > eps) for eps in epsilons]
+
+hoeffding_probs = hoeffding_bound(epsilons, FLIPS)
+
+
+def hoeffding_bound(epsilon, N):
+    return 2 * np.exp(-2 * epsilon**2 * N)
+
 
 for i in range(TRIALS):
     # Simulate flips for all coins
@@ -25,44 +37,6 @@ for i in range(TRIALS):
     # Record data for the coin with the minimum frequency of heads
     min_heads_coin_index = np.argmin(heads_counts)
     min_heads_coin_heads[i] = heads_counts[min_heads_coin_index]
-
-
-# Plotting the results
-#plt.figure(figsize=(15,5))
-#
-#plt.subplot(1, 3, 1)
-#plt.hist(first_coin_heads, bins=range(12), alpha=0.7, edgecolor='black')
-#plt.title("First Coin")
-#plt.xlabel("Number of Heads")
-#plt.ylabel("Frequency")
-#
-#plt.subplot(1, 3, 2)
-#plt.hist(random_coin_heads, bins=range(12), alpha=0.7, edgecolor='black')
-#plt.title("Randomly Chosen Coin")
-#plt.xlabel("Number of Heads")
-#
-#plt.subplot(1, 3, 3)
-#plt.hist(min_heads_coin_heads, bins=range(12), alpha=0.7, edgecolor='black')
-#plt.title("Coin with Minimum Heads")
-#plt.xlabel("Number of Heads")
-#
-#plt.tight_layout()
-#plt.show()
-
-
-
-def hoeffding_bound(epsilon, N):
-    return 2 * np.exp(-2 * epsilon**2 * N)
-
-epsilons = np.arange(0, 0.5, 0.01)
-
-first_coin_probs = [np.mean(np.abs(first_coin_heads/FLIPS - 0.5) > eps) for eps in epsilons]
-random_coin_probs = [np.mean(np.abs(random_coin_heads/FLIPS - 0.5) > eps) for eps in epsilons]
-min_heads_coin_probs = [np.mean(np.abs(min_heads_coin_heads/FLIPS - 0.5) > eps) for eps in epsilons]
-
-hoeffding_probs = hoeffding_bound(epsilons, FLIPS)
-
-
 
 
 # Plotting the results
