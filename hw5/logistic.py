d05c5e3746ee322a275388bbc830a9b87b612642
Lien Zhu <lienzhucs@gmail.com>
2023-10-31 18:45:05 -0700

    epochs is correct, E_out is not...

--- hw5/logistic.py
+++ hw5/logistic.py
@@ -7,6 +7,11 @@ import matplotlib.pyplot as plt
 import argparse
 
 
+TRIALS          = 100
+TEST_SAMPLES    = 1000
+ETA             = 0.01
+
+
 def generate_target():
     points = np.random.uniform(-1, 1, (2, 2))
     p0 = points[0]
@@ -24,13 +29,55 @@ def generate_data(N, a, b, c):
     return X_bias, Y
 
 
+
+def calc_gradient(w, x, y):
+    return -y*( 1 / (1 + np.exp(y * np.dot(w, x)) ) ) * x
+
+def logistic_regression(X, Y):
+    num_points = X.shape[0]
+    dim = X.shape[1]
+    w = np.zeros(dim)
+    w_prev = None
+    
+    epoch = 0
+    while w_prev is None or np.linalg.norm( w - w_prev ) > 0.01:
+        w_prev = w.copy()
+        shuffled_indices = np.random.permutation(num_points)
+        for i in shuffled_indices:
+            x, y = X[i], Y[i]
+            w += -ETA * calc_gradient(w, x, y)
+        epoch += 1
+
+    return w, epoch
+
+def calc_E_out(w_g, a, b, c, num_samples=TEST_SAMPLES):
+    X_test, Y_f = generate_data(num_samples, a, b, c)
+    Y_g = np.sign(X_test.dot(w_g))
+    E_out = np.mean(Y_f != Y_g)
+    return E_out
+
+
 def main():
     parser = argparse.ArgumentParser(description="Logistic Regression and Stochastic Gradient Descent Algorithm")
     parser.add_argument('-N', '--points', type=int, help='Number of sample data points to generate', required=True)
     args = parser.parse_args()
 
-    a, b, c = generate_target()
-    X, Y = generate_data(args.points, a, b, c)
+    epochs_list = []
+    list_of_E_outs = []
+
+    for _ in range(TRIALS):
+        a, b, c = generate_target()
+        X, Y = generate_data(args.points, a, b, c)
+        w_logistic, epochs = logistic_regression(X, Y)
+        epochs_list.append(epochs)
+        list_of_E_outs.append(calc_E_out(w_logistic, a, b, c))
+
+    avg_epochs = np.mean(epochs_list)
+    avg_E_out = np.mean(list_of_E_outs)
+
+    print("Epochs:\t", avg_epochs)
+    print("E_out:\t", avg_E_out)
+
 
     # plot data points
     plt.figure(figsize=(8, 6))
@@ -42,12 +89,16 @@ def main():
     y_vals = (-c - a*x_vals) / b
     plt.plot(x_vals, y_vals, 'k-', label='Target function f')
 
+    # plot the last chosen hypothesis g for visualization purposes
+    y_vals_g = (-w_logistic[0] - w_logistic[1]*x_vals) / w_logistic[2]
+    plt.plot(x_vals, y_vals_g, 'm--', label='Hypothesis g')
+
     plt.xlim(-1, 1)
     plt.ylim(-1, 1)
     plt.axhline(0, color='black',linewidth=0.5)
     plt.axvline(0, color='black',linewidth=0.5)
     plt.legend()
-    plt.title("Sample Data and Target Function f")
+    plt.title("Logistic Regression")
     plt.xlabel('x1')
     plt.ylabel('x2')
     plt.grid(True)
