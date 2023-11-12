514cd015eec4ccbe5a423b50d3bcd01a05e0e7db
Lien Zhu <lienzhucs@gmail.com>
2023-11-12 01:37:01 -0800

    implements class vs class

--- hw8/poly_svm.py
+++ hw8/poly_svm.py
@@ -4,6 +4,7 @@
 
 import numpy as np
 from sklearn import svm
+import argparse
 
 
 
@@ -16,12 +17,19 @@ TEST_DATA  = "./data/features.test"
 ### Data Processing ###
 #######################
 
-def load_data(file_path, digit):
+def load_data(file_path, digit, other_digit=None):
     data = np.loadtxt(file_path)
-    Y = data[ : , 0]
-    Y = np.where(Y == digit, 1, -1)
-    X = data[ : , 1: ]
-    
+    Y = data[:, 0]
+    X = data[:, 1:]
+
+    if other_digit is not None:
+        mask = (Y == digit) | (Y == other_digit)
+        Y = Y[mask]
+        X = X[mask]
+        Y = np.where(Y == digit, 1, -1)
+    else:
+        Y = np.where(Y == digit, 1, -1)
+
     return X, Y
 
 
@@ -57,20 +65,52 @@ def calc_E_cv(model, X, Y):
 #################
 
 def main():
-
-    C_list = [0.001, 0.01, 0.1, 1]
-
-    for digit in range(0,10):
-        X, Y = load_data(TRAIN_DATA, digit)
-        model = svm_libsvm(X, Y)
-        num_alphas = sum(model.n_support_)
-
-        E_in = calc_e(model, X, Y)
-        X_test, Y_test = load_data(TEST_DATA, digit)
-        E_out = calc_e(model, X_test, Y_test)
-
-        print(f"{digit} versus all  E_in: {E_in:.5f}  E_out: {E_out:.5f}  SVs: {round(num_alphas)}")
+    parser = argparse.ArgumentParser(description="Polynomial Kernel Soft Margin SVM")
+    parser.add_argument('-d', '--digit', type=int, help='First digit class')
+    parser.add_argument('-o', '--other', type=int, help='Second digit class')
+    args = parser.parse_args()
         
+    if args.digit and args.other:
+
+        C_list = [0.0001, 0.001, 0.01, 0.1, 1]
+
+        print("Q =", 2)
+        for C in C_list:
+            X, Y = load_data(TRAIN_DATA, args.digit, args.other)
+            model = svm_libsvm(X, Y, C=C, Q=2)
+            num_alphas = sum(model.n_support_)
+
+            E_in = calc_e(model, X, Y)
+            X_test, Y_test = load_data(TEST_DATA, 1, 5)
+            E_out = calc_e(model, X_test, Y_test)
+
+            print(f"C = {C:.4f}\t{args.digit} versus {args.other}  E_in: {E_in:.5f}  E_out: {E_out:.5f}  SVs: {round(num_alphas)}")
+        print()
+
+        print("Q =", 5)
+        for C in C_list:
+            X, Y = load_data(TRAIN_DATA, args.digit, args.other)
+            model = svm_libsvm(X, Y, C=C, Q=5)
+            num_alphas = sum(model.n_support_)
+
+            E_in = calc_e(model, X, Y)
+            X_test, Y_test = load_data(TEST_DATA, 1, 5)
+            E_out = calc_e(model, X_test, Y_test)
+
+            print(f"C = {C:.4f}\t{args.digit} versus {args.other}  E_in: {E_in:.5f}  E_out: {E_out:.5f}  SVs: {round(num_alphas)}")
+
+    else:
+        
+        for digit in range(0,10):
+            X, Y = load_data(TRAIN_DATA, digit)
+            model = svm_libsvm(X, Y)
+            num_alphas = sum(model.n_support_)
+
+            E_in = calc_e(model, X, Y)
+            X_test, Y_test = load_data(TEST_DATA, digit)
+            E_out = calc_e(model, X_test, Y_test)
+
+            print(f"{digit} versus all  E_in: {E_in:.5f}  E_out: {E_out:.5f}  SVs: {round(num_alphas)}")
 
 
 if __name__ == "__main__":
